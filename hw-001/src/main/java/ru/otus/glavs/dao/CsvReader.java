aa3e871ca32f219cc3dfbe2d31f60a890fb1796b
GlavS <pianoforte2@yandex.ru>
2022-11-25 12:42:35 +0300

    DAO implementation scaffold created

---
+++ hw-001/src/main/java/ru/otus/glavs/dao/CsvReader.java
@@ -0,0 +1,13 @@
+package ru.otus.glavs.dao;
+
+import ru.otus.glavs.domain.Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+@SuppressWarnings("unused")
+public class CsvReader {
+    public List<Quiz> readAllRows(){
+        List<Quiz> result = new ArrayList<>();
+        return result;
+    }
+}
