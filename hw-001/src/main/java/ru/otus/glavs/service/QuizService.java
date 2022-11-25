c04b9870e93371a0147cf534f26b26d152feabdc
GlavS <pianoforte2@yandex.ru>
2022-11-25 13:27:03 +0300

    Service beans added to context

--- hw-001/src/main/java/ru/otus/glavs/service/QuizService.java
+++ hw-001/src/main/java/ru/otus/glavs/service/QuizService.java
@@ -1,6 +1,14 @@
 package ru.otus.glavs.service;
 
+import ru.otus.glavs.dao.QuizDao;
+
 public class QuizService {
+    private QuizDao dao;
+
+    public QuizService(QuizDao dao) {
+        this.dao = dao;
+    }
+
     public void displayAll(){
 
     }
