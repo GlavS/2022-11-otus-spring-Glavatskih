e5f7aff3297f093c2b795704dd8a4cf322722b6b
GlavS <pianoforte2@yandex.ru>
2022-11-25 13:02:06 +0300

    Bean csvReader created, property set

--- hw-001/src/main/java/ru/otus/glavs/dao/CsvReader.java
+++ hw-001/src/main/java/ru/otus/glavs/dao/CsvReader.java
@@ -6,8 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 @SuppressWarnings("unused")
 public class CsvReader {
+    private String quizCsvFileName; // property, hardcoded in spring-context.xml
     public List<Quiz> readAllRows(){
         List<Quiz> result = new ArrayList<>();
         return result;
+    }
+
+    public void setQuizCsvFileName(String quizCsvFileName) { //property setter
+        this.quizCsvFileName = quizCsvFileName;
     }
 }
