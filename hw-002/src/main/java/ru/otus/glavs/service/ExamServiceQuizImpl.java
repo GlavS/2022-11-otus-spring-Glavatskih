e85c4aef297f09708d55f07fbfc06e4d2a82d80e
GlavS <pianoforte2@yandex.ru>
2022-12-03 23:46:14 +0300

    Refactor ExamService class

--- hw-002/src/main/java/ru/otus/glavs/service/ExamServiceQuizImpl.java
+++ hw-002/src/main/java/ru/otus/glavs/service/ExamServiceQuizImpl.java
@@ -4,9 +4,9 @@ import ru.otus.glavs.domain.Quiz;
 import ru.otus.glavs.domain.Student;
 import ru.otus.glavs.service.helper.ConsoleHelper;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ExamServiceQuizImpl implements ExamService {
 
@@ -23,18 +23,23 @@ public class ExamServiceQuizImpl implements ExamService {
 
     @Override
     public void examine() {
-        Map<Integer, Answer> answerList = new HashMap<>();
-
-        ch.writeMessage("Starting our exam...");
         Student student = studentService.register();
-        List<Quiz> questionList = quizService.getQuestionList();
+        printGreeting();
+        Map<Integer, Answer> answerMap = makeAnswerMap();
+        printExamResults(student, answerMap);
+    }
 
+    private void printGreeting() {
+        ch.writeMessage("Starting our exam...");
         ch.writeMessage("==========================================");
         ch.writeMessage("Examination rules: read question and pick one of three answers");
         ch.writeMessage("by typing 1, 2 or 3");
         ch.writeMessage(System.lineSeparator());
+    }
 
-
+    private Map<Integer, Answer> makeAnswerMap() {
+        Map<Integer, Answer> answerMap = new TreeMap<>();
+        List<Quiz> questionList = quizService.getQuestionList();
         for (Quiz question :
                 questionList) {
             quizService.displayQuestion(question);
@@ -45,22 +50,26 @@ public class ExamServiceQuizImpl implements ExamService {
                 answer = ch.readInt();
             }
             boolean isCorrect = (answer == question.getCorrectAnswer());
-            answerList.put(question.getId(), new Answer(answer, isCorrect));
+            answerMap.put(question.getId(), new Answer(answer, isCorrect));
         }
+        return answerMap;
+    }
 
+    private void printExamResults(Student student, Map<Integer, Answer> answerMap) {
         ch.writeMessage("Exam results review:");
-        int correctAnswerCount = (int) answerList.values().stream().filter(ans -> ans.isCorrect).count();
+        int correctAnswerCount = (int) answerMap.values().stream().filter(ans -> ans.isCorrect).count();
         boolean isPassed = (correctAnswerCount > 3);
-        String examResult = isPassed? "Exam is passed" : "Exam is not passed";
+        String examResult = isPassed ? "Exam is passed" : "Exam is not passed";
         ch.writeMessage(examResult);
         ch.write(String.format("%s %s correctly answered %d questions of 5", student.getName(), student.getSurname(), correctAnswerCount));
         ch.writeMessage(System.lineSeparator());
     }
-    private static class Answer{
-        int variant;
-        boolean isCorrect;
 
-        public Answer(int variant, boolean isCorrect) {
+    private static final class Answer {
+        private final int variant;
+        private final boolean isCorrect;
+
+        private Answer(int variant, boolean isCorrect) {
             this.variant = variant;
             this.isCorrect = isCorrect;
         }
