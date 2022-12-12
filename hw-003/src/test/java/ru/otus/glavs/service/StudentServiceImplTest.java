78ecbea8a3349a32035d8d275e2b499fc31f3490
GlavS <pianoforte2@yandex.ru>
2022-12-12 20:08:04 +0300

    StudentService class localized

--- hw-003/src/test/java/ru/otus/glavs/service/StudentServiceImplTest.java
+++ hw-003/src/test/java/ru/otus/glavs/service/StudentServiceImplTest.java
@@ -2,9 +2,13 @@ package ru.otus.glavs.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.glavs.dao.StudentDao;
 import ru.otus.glavs.dao.StudentDaoImpl;
 import ru.otus.glavs.domain.Student;
+import ru.otus.glavs.l10n.LocalizedStudentServiceMessageStorage;
 import ru.otus.glavs.service.helper.ConsoleHelperImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,14 +17,21 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @DisplayName("StudentServiceImpl class")
+@ExtendWith(MockitoExtension.class)
 class StudentServiceImplTest {
-    private final StudentDao dao = mock(StudentDaoImpl.class);
-    private final ConsoleHelperImpl ch = mock(ConsoleHelperImpl.class);
+    @Mock
+    private StudentDao dao;
+    @Mock
+    private ConsoleHelperImpl ch;
+    @Mock
+    private LocalizedStudentServiceMessageStorage storage;
 
     @Test
     @DisplayName("register() method should return Student object instance")
     void registerMethodShouldReturnStudentInstance() {
-        StudentServiceImpl studentService = new StudentServiceImpl(dao, ch);
+        StudentServiceImpl studentService = new StudentServiceImpl(dao, ch, storage);
+        when(storage.getRegisterFirstNamePrompt()).thenReturn("First name prompt");
+        when(storage.getRegisterLastNamePrompt()).thenReturn("Last name prompt");
 
         String name = "Name";
         String surname = "Surname";
