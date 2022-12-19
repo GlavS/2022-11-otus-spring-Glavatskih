9efb8a4a93293fb687e0e2efd9a540d7857b3c02
GlavS <pianoforte2@yandex.ru>
2022-12-19 23:34:46 +0300

    Old tests deleted

--- hw-004/src/main/java/ru/otus/glavs/l10n/LocalizedMessagesStorage.java
+++ hw-004/src/main/java/ru/otus/glavs/l10n/LocalizedMessagesStorage.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Component;
 import ru.otus.glavs.properties.Application;
 
 import java.util.Locale;
+
 @Component
-public class LocalizedMessagesStorage implements LocalizedMessages{
+public class LocalizedMessagesStorage implements LocalizedMessages {
     protected final MessageSource locMessage;
     protected final Locale locale;
+
     public LocalizedMessagesStorage(Application props, MessageSource locMessage) {
         this.locMessage = locMessage;
         this.locale = props.getLocale();
