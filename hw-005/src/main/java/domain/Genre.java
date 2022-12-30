f3d4ac1412887cf14345339047fd742c00e82b20
GlavS <pianoforte2@yandex.ru>
2022-12-31 00:53:32 +0300

    Domain model created

---
+++ hw-005/src/main/java/domain/Genre.java
@@ -0,0 +1,12 @@
+package domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+@Data
+@RequiredArgsConstructor
+public class Genre {
+    private final int id;
+    private final String genre;
+}
