c519351521c0e2285123348627d43b3fd585bf50
GlavS <pianoforte2@yandex.ru>
2022-12-31 00:34:55 +0300

    Test database configured

---
+++ hw-005/src/test/resources/testdata.sql
@@ -0,0 +1,17 @@
+INSERT INTO BOOKS (AUTHOR_ID, GENRE_ID, NAME)
+VALUES (1, 1, 'Книга1'),
+       (1, 1, 'Книга2'),
+       (2, 1, 'Книга3'),
+       (2, 2, 'Книга4');
+
+
+INSERT INTO GENRES (GENRE)
+VALUES ('Жанр1'),
+       ('Жанр2');
+
+
+
+INSERT INTO AUTHORS (NAME, SURNAME, INITIALS)
+VALUES ('Имя1', 'Фамилия1', 'А.А.'),
+       ('Имя2', 'Фамилия2', 'Б.Б.');
+
