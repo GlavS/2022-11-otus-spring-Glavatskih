62acc049a89f134f5cf22c73151c81cee24964de
GlavS <pianoforte2@yandex.ru>
2023-01-04 01:23:02 +0300

    all methods in AuthorsDao implemented

--- hw-005/src/main/java/ru/glavs/hw005/dao/AuthorDaoImpl.java
+++ hw-005/src/main/java/ru/glavs/hw005/dao/AuthorDaoImpl.java
@@ -1,7 +1,11 @@
 package ru.glavs.hw005.dao;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.glavs.hw005.domain.Author;
 
@@ -9,6 +13,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.Objects.isNull;
 
 @Repository
 public class AuthorDaoImpl implements AuthorDao {
@@ -31,17 +37,29 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public int count() {
-        return 0;
+        Integer result = jdbc
+                .getJdbcOperations()
+                .queryForObject("SELECT COUNT(*) FROM AUTHORS", Integer.class);
+        return isNull(result)? 0 : result;
     }
 
     @Override
     public int insertNew(Author author) {
-        return 0;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("NAME", author.getName())
+                .addValue("SURNAME", author.getSurname())
+                .addValue("INITIALS", author.getInitials());
+        jdbc.update("INSERT INTO AUTHORS(NAME, SURNAME, INITIALS) VALUES(:NAME, :SURNAME, :INITIALS)",
+                params, keyHolder);
+        Integer key = (Integer) keyHolder.getKey();
+        return isNull(key)? 0 : key;
     }
 
     @Override
     public void delete(Author author) {
-
+        Map<String, Integer> param = Map.of("ID", author.getId());
+        jdbc.update("DELETE FROM AUTHORS WHERE ID = :ID", param);
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
