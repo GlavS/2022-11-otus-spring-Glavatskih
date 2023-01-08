cd7e83d34db551ab189a0f18288e3c03c3629802
GlavS <pianoforte2@yandex.ru>
2023-01-08 23:11:43 +0300

    AuthorDisplayServiceTest done

--- hw-005/src/test/java/ru/glavs/hw005/service/display/AuthorDisplayServiceTest.java
+++ hw-005/src/test/java/ru/glavs/hw005/service/display/AuthorDisplayServiceTest.java
@@ -1,33 +1,65 @@
 package ru.glavs.hw005.service.display;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import ru.glavs.hw005.dao.AuthorDao;
 import ru.glavs.hw005.dao.AuthorDaoImpl;
+import ru.glavs.hw005.domain.Author;
+import ru.glavs.hw005.io.StreamIOService;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
-@DisplayName("NOT IMPLEMENTED!!")
+@DisplayName("В классе AuthorDisplayService")
 class AuthorDisplayServiceTest {
+    private final List<Author> testList = List.of(
+            new Author(1, "Имя1", "Фамилия1", "А.А."),
+            new Author(2, "Имя2", "Фамилия2", "Б.Б.")
+    );
+    @MockBean
+    StreamIOService ioService;
     @Autowired
     private AuthorDisplayService service;
-    @Autowired
-    private AuthorDaoImpl authorDao;
+
 
     @Test
-    @DisplayName("NOT IMPLEMENTED!!")
-        //TODO:!!!!
-    void printOne() {
+    @DisplayName("метод printOne корректно вызывает методы интерфейса IOService")
+    void printOneMethodShouldCorrectlyInvokeIOServiceMethods() {
+        service.printOne(testList.get(0));
+        verify(ioService, times(3)).println(anyString());
+        verify(ioService, times(2)).printf(anyString(), any());
     }
 
     @Test
-    @DisplayName("NOT IMPLEMENTED!!")
-    void printList() {
-        service.printList(authorDao.getAll());
+    @DisplayName("метод printList корректно вызывает методы интерфейса IOService")
+    void printListMethodShouldCorrectlyInvokeIOServiceMethods() {
+        service.printList(testList);
+        verify(ioService, times(3)).println(anyString());
+        verify(ioService, times(3)).printf(anyString(), any());
     }
 
     @Test
-    @DisplayName("NOT IMPLEMENTED!!")
-    void displayItem() {
+    @DisplayName("метод displayItem корректно вызывает методы интерфейса IOService")
+    void displayItemMethodShouldCorrectlyInvokeIOServiceMethods() {
+        service.displayItem(testList.get(0));
+        verify(ioService, times(1)).printf(anyString(), any());
+    }
+    @Configuration
+    @ComponentScan({"ru.glavs.hw005.service.display", "ru.glavs.hw005.io"})
+    static class TestConfig{
+
     }
 }
