bba9351d068e8ff808d86f0dd4e768bca07795ad
GlavS <pianoforte2@yandex.ru>
2023-01-23 00:40:05 +0300

    Comment display service started

--- hw-006/src/main/java/ru/glavs/hw005/service/display/CommentDisplayService.java
+++ hw-006/src/main/java/ru/glavs/hw005/service/display/CommentDisplayService.java
@@ -1,2 +1,16 @@
-package ru.glavs.hw005.service.display;public class CommentDisplayService {
+package ru.glavs.hw005.service.display;
+
+import ru.glavs.hw005.domain.Comment;
+import ru.glavs.hw005.io.IOService;
+
+public class CommentDisplayService extends AbstractDisplayService<Comment> implements DisplayService<Comment> {
+    public CommentDisplayService(IOService ioService) {
+        super(ioService);
+    }
+
+    @Override
+    protected void displayItem(Comment item) {
+
+    }
+
 }
