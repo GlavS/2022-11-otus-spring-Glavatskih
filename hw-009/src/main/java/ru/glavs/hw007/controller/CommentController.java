c17abcf08b8347deaec655cb10eb6771b40aa444
GlavS <pianoforte2@yandex.ru>
2023-02-16 23:32:31 +0300

    Comment update controller method implemented

--- hw-009/src/main/java/ru/glavs/hw007/controller/CommentController.java
+++ hw-009/src/main/java/ru/glavs/hw007/controller/CommentController.java
@@ -4,9 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.glavs.hw007.domain.Comment;
 import ru.glavs.hw007.service.CRUD.CommentCRUD;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @AllArgsConstructor
@@ -19,5 +24,27 @@ public class CommentController {
         Comment comment = commentCRUDservice.findById(id);
         model.addAttribute("comment", comment);
         return "edit-comment";
+    }
+
+    @PostMapping("/comment/edit")
+    public String updateComment(
+            @RequestParam long id,
+            @RequestParam String text,
+            @RequestParam String authorNick,
+            @RequestParam String date
+    ){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Comment comment = commentCRUDservice.findById(id);
+        Date dateReceived;
+        try {
+            dateReceived = format.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException("Cannot parse comment date: " + e.getMessage(), e);
+        }
+        comment.setText(text);
+        comment.setAuthorNick(authorNick);
+        comment.setDate(dateReceived);
+        commentCRUDservice.save(comment);
+        return "redirect:/"; //TODO: redirect to book edit page
     }
 }
