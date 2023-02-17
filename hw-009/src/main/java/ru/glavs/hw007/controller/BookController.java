5c88e76258887fe423d71817783683723a8f95e1
GlavS <pianoforte2@yandex.ru>
2023-02-17 17:00:55 +0300

    Book update method created

--- hw-009/src/main/java/ru/glavs/hw007/controller/BookController.java
+++ hw-009/src/main/java/ru/glavs/hw007/controller/BookController.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.glavs.hw007.domain.Book;
 import ru.glavs.hw007.domain.Comment;
 import ru.glavs.hw007.service.CRUD.BookCRUD;
@@ -17,16 +19,32 @@ public class BookController {
     private final BookCRUD bookCRUDservice;
 
     @GetMapping("/")
-    public String bookListPage(Model model){
+    public String bookListPage(Model model) {
         List<Book> bookList = bookCRUDservice.readAll();
         model.addAttribute("books", bookList);
         return "list";
     }
 
-    @GetMapping("/edit")
-    public String editBookPage(@RequestParam long id, Model model){
+    @GetMapping("/book/show")
+    public String showBookPage(@RequestParam long id, Model model) {
         Book book = bookCRUDservice.readBook(id);
         model.addAttribute("book", book);
         return "book";
+    }
+
+    @GetMapping("/book/edit")
+    public String updateBookPage(@RequestParam long id, Model model) {
+        Book book = bookCRUDservice.readBook(id);
+        model.addAttribute("book", book);
+        return "edit-book";
+    }
+    @PostMapping("/book/edit")
+    public String updateBook(Book bookUpdated, RedirectAttributes attributes){
+        long id = bookUpdated.getId();
+        Book book = bookCRUDservice.readBook(id);
+        bookUpdated.setComments(book.getComments());
+        attributes.addAttribute("id", book.getId());
+        bookCRUDservice.save(bookUpdated);
+        return "redirect:/book/show";
     }
 }
--- hw-009/src/main/java/ru/glavs/hw007/controller/CommentController.java
+++ hw-009/src/main/java/ru/glavs/hw007/controller/CommentController.java
@@ -3,11 +3,9 @@ package ru.glavs.hw007.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.glavs.hw007.domain.Comment;
 import ru.glavs.hw007.service.CRUD.CommentCRUD;
@@ -23,7 +21,7 @@ public class CommentController {
     private CommentCRUD commentCRUDservice;
 
     @GetMapping("/comment/edit")
-    public String editCommentPage(@RequestParam long id, Model model){
+    public String editCommentPage(@RequestParam long id, Model model) {
         Comment comment = commentCRUDservice.findById(id);
         model.addAttribute("comment", comment);
         return "edit-comment";
@@ -34,30 +32,36 @@ public class CommentController {
             @RequestParam long id,
             @RequestParam String text,
             @RequestParam String authorNick,
-            @RequestParam String date,
+            @RequestParam("date") String dateString,
             RedirectAttributes attributes
-    ){
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    ) {
         Comment comment = commentCRUDservice.findById(id);
+        Date date = parseDate(dateString);
+        comment.setText(text);
+        comment.setAuthorNick(authorNick);
+        comment.setDate(date);
+        commentCRUDservice.save(comment);
+        attributes.addAttribute("id", comment.getCommentedBook().getId());
+        return "redirect:/book/show";
+    }
+
+    @GetMapping("/comment/delete")
+    public String deleteComment(@RequestParam long id, RedirectAttributes attributes) {
+        Comment comment = commentCRUDservice.findById(id);
+        attributes.addAttribute("id", comment.getCommentedBook().getId());
+        commentCRUDservice.delete(comment);
+        return "redirect:/book/show";
+    }
+
+    private Date parseDate(String date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Date dateReceived;
         try {
             dateReceived = format.parse(date);
         } catch (ParseException e) {
             throw new RuntimeException("Cannot parse comment date: " + e.getMessage(), e);
         }
-        comment.setText(text);
-        comment.setAuthorNick(authorNick);
-        comment.setDate(dateReceived);
-        commentCRUDservice.save(comment);
-        attributes.addAttribute("id", comment.getCommentedBook().getId());
-        return "redirect:/edit";
-    }
-    @GetMapping("/comment/delete")
-    public String deleteComment(@RequestParam long id, RedirectAttributes attributes){
-        Comment comment = commentCRUDservice.findById(id);
-        attributes.addAttribute("id",comment.getCommentedBook().getId());
-        commentCRUDservice.delete(comment);
-        return "redirect:/edit";
+        return dateReceived;
     }
 
 }
