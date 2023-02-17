71c71667f3a754c6dfd43a8e9bb015463129f93d
GlavS <pianoforte2@yandex.ru>
2023-02-17 23:52:16 +0300

    Update view and logic updated

--- hw-009/src/main/java/ru/glavs/hw007/controller/BookController.java
+++ hw-009/src/main/java/ru/glavs/hw007/controller/BookController.java
@@ -7,44 +7,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.glavs.hw007.domain.Author;
 import ru.glavs.hw007.domain.Book;
-import ru.glavs.hw007.domain.Comment;
+import ru.glavs.hw007.domain.Genre;
+import ru.glavs.hw007.service.CRUD.AuthorCRUD;
 import ru.glavs.hw007.service.CRUD.BookCRUD;
+import ru.glavs.hw007.service.CRUD.GenreCRUD;
 
 import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class BookController {
-    private final BookCRUD bookCRUDservice;
+    private final BookCRUD bookCRUDService;
+    private final AuthorCRUD authorCRUDService;
+    private final GenreCRUD genreCRUDService;
 
     @GetMapping("/")
     public String bookListPage(Model model) {
-        List<Book> bookList = bookCRUDservice.readAll();
+        List<Book> bookList = bookCRUDService.readAll();
         model.addAttribute("books", bookList);
         return "list";
     }
 
     @GetMapping("/book/show")
     public String showBookPage(@RequestParam long id, Model model) {
-        Book book = bookCRUDservice.readBook(id);
+        Book book = bookCRUDService.readBook(id);
         model.addAttribute("book", book);
         return "book";
     }
 
     @GetMapping("/book/edit")
     public String updateBookPage(@RequestParam long id, Model model) {
-        Book book = bookCRUDservice.readBook(id);
+        Book book = bookCRUDService.readBook(id);
+        List<Author> authorList = authorCRUDService.findAll();
+        List<Genre> genreList = genreCRUDService.findAll();
         model.addAttribute("book", book);
+        model.addAttribute("authors", authorList);
+        model.addAttribute("genres", genreList);
         return "edit-book";
     }
+
     @PostMapping("/book/edit")
-    public String updateBook(Book bookUpdated, RedirectAttributes attributes){
+    public String updateBook(Book bookUpdated, RedirectAttributes attributes) {
         long id = bookUpdated.getId();
-        Book book = bookCRUDservice.readBook(id);
+        Book book = bookCRUDService.readBook(id);
         bookUpdated.setComments(book.getComments());
         attributes.addAttribute("id", book.getId());
-        bookCRUDservice.save(bookUpdated);
+        bookCRUDService.save(bookUpdated);
         return "redirect:/book/show";
     }
 }
