f662af5449bc5ca395e3e4d8c003ecac41ffeb4f
GlavS <pianoforte2@yandex.ru>
2023-03-10 23:16:28 +0300

    Sample router functions implemented

--- hw-011/src/main/java/ru/glavs/hw008/Hw011Application.java
+++ hw-011/src/main/java/ru/glavs/hw008/Hw011Application.java
@@ -1,39 +1,20 @@
 package ru.glavs.hw008;
 
-//import com.github.cloudyrock.spring.v5.EnableMongock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import ru.glavs.hw008.domain.Author;
-import ru.glavs.hw008.repository.AuthorRepository;
-import ru.glavs.hw008.repository.BookRepository;
-import ru.glavs.hw008.repository.CommentRepository;
-import ru.glavs.hw008.repository.GenreRepository;
 
 @EnableWebFlux
 @EnableReactiveMongoRepositories
 @SpringBootApplication
 public class Hw011Application {
 
-    public static void main(String[] args) throws Exception{
-        ConfigurableApplicationContext context = SpringApplication.run(Hw011Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Hw011Application.class);
         System.out.println("Main page: http://localhost:8080");
 
-        AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
-        GenreRepository genreRepository = context.getBean(GenreRepository.class);
-        BookRepository bookRepository = context.getBean(BookRepository.class);
-        CommentRepository commentRepository = context.getBean(CommentRepository.class);
-//        Author author = new Author("Alex", "Pushkin", "A.S.");
-//        Mono<Author> monoAuthor = repository.save(author);
-        Thread.sleep(3000);
-        authorRepository.findAll().subscribe(System.out::println);
-        genreRepository.findAll().subscribe(System.out::println);
-        bookRepository.findAll().subscribe(System.out::println);
-        commentRepository.findAll().subscribe(System.out::println);
 
     }
 }
--- hw-011/src/main/java/ru/glavs/hw008/page/BookPageController.java
+++ hw-011/src/main/java/ru/glavs/hw008/page/BookPageController.java
@@ -1,31 +1,31 @@
-package ru.glavs.hw008.page;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class BookPageController {
-
-
-    @GetMapping("/")
-    public String listBooksPage() {
-        return "list";
-    }
-
-    @GetMapping("/book-edit")
-    public String bookEditPage(@RequestParam String id, Model model){
-        model.addAttribute("bookId", id);
-        return "edit/book-edit";
-    }
-    @GetMapping("/book-show")
-    public String bookShowPage(@RequestParam String id, Model model){
-        model.addAttribute("bookId", id);
-        return "show";
-    }
-    @GetMapping("/book-create")
-    public String bookCreatePage(){
-        return "create/book-create";
-    }
-}
+//package ru.glavs.hw008.page;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.reactive.result.view.Rendering;
+//import reactor.core.publisher.Mono;
+//
+//@Controller
+//public class BookPageController {
+//
+//
+//    @GetMapping("/")
+//    public Mono<Rendering> listBooksPage() {
+//        return Mono.just(Rendering.view("list").build());
+//    }
+//
+////    @GetMapping("/book-edit")
+////    public String bookEditPage(@RequestParam String id, Model model){
+////        model.addAttribute("bookId", id);
+////        return "edit/book-edit";
+////    }
+////    @GetMapping("/book-show")
+////    public String bookShowPage(@RequestParam String id, Model model){
+////        model.addAttribute("bookId", id);
+////        return "show";
+////    }
+////    @GetMapping("/book-create")
+////    public String bookCreatePage(){
+////        return "create/book-create";
+////    }
+//}
