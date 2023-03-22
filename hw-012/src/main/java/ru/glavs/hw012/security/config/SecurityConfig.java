3d1130eddf3b69123caa905f86f9483b4bb16410
GlavS <pianoforte2@yandex.ru>
2023-03-22 15:13:10 +0300

    Security configuration done

---
+++ hw-012/src/main/java/ru/glavs/hw012/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.glavs.hw012.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        return http.build();
+    }
+}
