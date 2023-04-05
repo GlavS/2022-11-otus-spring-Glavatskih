87c0ec652fd88467648516050fb078a7de4cb2fc
GlavS <pianoforte2@yandex.ru>
2023-04-05 14:18:39 +0300

    Batch config created

--- hw-014/src/main/java/ru/glavs/hw014/batch/BookProcessor.java
+++ hw-014/src/main/java/ru/glavs/hw014/batch/BookProcessor.java
@@ -1,2 +1,34 @@
-package ru.glavs.hw014.batch;public class BookProcessor {
+package ru.glavs.hw014.batch;
+
+import org.bson.types.ObjectId;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+import ru.glavs.hw014.batch.mongo.MongoBook;
+import ru.glavs.hw014.batch.mongo.MongoComment;
+import ru.glavs.hw014.domain.Book;
+
+import java.util.List;
+
+@Component
+public class BookProcessor implements ItemProcessor<Book, MongoBook> {
+    @Override
+    public MongoBook process(Book book) {
+        List<MongoComment> commentList = book.getComments()
+                .stream()
+                .map(comment ->
+                        new MongoComment(
+                                new ObjectId(),
+                                comment.getText(),
+                                comment.getAuthorNick(),
+                                comment.getDate()))
+                .toList();
+        return new MongoBook(
+                new ObjectId(),
+                book.getAuthor(),
+                book.getGenre(),
+                book.getTitle(),
+                commentList
+        );
+    }
 }
